7c06c0ebef8c273ab7ce69bd515930ed296887ea
mm <maciej.markiewicz10@gmail.com>
2021-12-02 21:30:09 +0100

    register

--- src/app/pages/registerpage/registerpage.component.ts
+++ src/app/pages/registerpage/registerpage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AuthService} from "../../auth/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registerpage',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterpageComponent implements OnInit {
 
-  constructor() { }
+
+  isLoggedIn=false;
+
+
+  registerForm: FormGroup;
+
+
+  constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router ) {
+    this.registerForm = this.formBuilder.group({
+      email: [''],
+      username: [''],
+      password: ['']
+    });
+  }
 
   ngOnInit(): void {
+
+    this.isLoggedIn=this.authService.isLoggedIn()
+
+
   }
+
+
+
+
+  logout():void
+  {
+    this.authService.logout();
+    window.location.reload();
+  }
+
+
+
+  get f() { return this.registerForm.controls; }
+
+  register() {
+    this.authService.register(
+      {
+        email: this.f.email.value,
+        username: this.f.username.value,
+        password: this.f.password.value
+      }
+    )
+      .subscribe(success => {
+        if (success) {
+          window.location.reload();
+        }
+      });
+  }
+
 
 }
